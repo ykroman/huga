38c63ed1cd8e832ef8f8d8c963a2c3d5b1318a82
ykroman <wsroman.rdky@gmail.com>
2020-06-28 13:44:01 +0900

    gradleプロジェクト一式

---
+++ HogeTest.java
@@ -0,0 +1,17 @@
+ackage huga;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HogeTest {
+    @Test
+    public void testAdd() {
+        assertEquals(3, 3);
+    }
+
+    @Test
+    public void testEqual() {
+        assertEquals("test", "test");
+    }
+    
+}
+
